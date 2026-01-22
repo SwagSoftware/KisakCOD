d778fc12364317e4a5daa514228dbeb27f642113
kiwidoggie <forums@allenthinks.com>
2026-01-22 08:22:06 -0500

    Update clang build.

--- scripts/platform/win32/platform.cmake
+++ scripts/platform/win32/platform.cmake
@@ -11,8 +11,8 @@ apply_platform_overrides(CLIENT_MP "${PLATFORM_OVERRIDE_DIR}")
 
 # Handle the platform specific things FIRST
 set_property( DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY VS_STARTUP_PROJECT ${BIN_NAME} )
-set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MT /O2 /Ot /MP /W3 /Zi ${MSVC_WARNING_DISABLES} /permissive-")
-set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /MTd /Od /MP /W3 /Zi ${MSVC_WARNING_DISABLES} /permissive-")
+set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MT /O2 /Ot /MP /W3 /Zi ${MSVC_WARNING_DISABLES} /permissive- /clang:-ftrivial-auto-var-init=zero")
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /MTd /Od /MP /W3 /Zi ${MSVC_WARNING_DISABLES} /permissive- /clang:-ftrivial-auto-var-init=zero")
 
 #target_compile_options(${PROJECT_NAME} PRIVATE /we4700)
 add_compile_options(/we4700)
